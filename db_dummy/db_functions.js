7f9adbf990d8c770b356d99d076dddcee01775a7
Raynier Chua <raynier_timothy_chua@dlsu.edu.ph>
2022-02-10 18:16:08 +0800

    removed unnecessary code from insert function

--- db_dummy/db_functions.js
+++ db_dummy/db_functions.js
@@ -61,48 +61,33 @@ function insertOneRecordIntoAllNodes(name, year, genre, director){
     values = [name, year, genre, director];
 
     // insert into node 1
-    con1.connect(function(err){
+    con1.query("INSERT INTO movies_all (name, year, genre, director) VALUES (?,?,?,?);", values, function(err, results){
         if(err) throw err;
-        console.log("Node 1: Connected");
+        console.log("Node 1: 1 record inserted");
 
-        con1.query("INSERT INTO movies_all (name, year, genre, director) VALUES (?,?,?,?);", values, function(err, results){
-            if(err) throw err;
-            console.log("Node 1: 1 record inserted");
-
-            // append auto-generated id to values array
-            values.unshift(results.insertId);
-            
-            // insert into node 2
-            if(year < 1980){
-                con2.connect(function(err){
-                    if(err) throw err;
-                    console.log("Node 2: Connected");
-
-                    con2.query("INSERT INTO movies_pre1980 (id, name, year, genre, director) VALUES (?,?,?,?,?);", values, function(err){
-                        if(err) throw err;
-                        console.log("Node 2: 1 record inserted");
-                        closeConnection(con2);
-                    });
-                });
-            };
+        // append auto-generated id to values array
+        values.unshift(results.insertId);
+        
+        // insert into node 2
+        if(year < 1980){
+            con2.query("INSERT INTO movies_pre1980 (id, name, year, genre, director) VALUES (?,?,?,?,?);", values, function(err){
+                if(err) throw err;
+                console.log("Node 2: 1 record inserted");
+                closeConnection(con2);
+            });
+        };
 
 
-            // insert into node 3
-            if(year >= 1980){
-                con3.connect(function(err){
-                    if(err) throw err;
-                    console.log("Node 3: Connected");
+        // insert into node 3
+        if(year >= 1980){
+            con3.query("INSERT INTO movies_post1980 (id, name, year, genre, director) VALUES (?,?,?,?,?);", values, function(err){
+                if(err) throw err;
+                console.log("Node 3: 1 record inserted");
+                closeConnection(con3);
+            });
+        };
 
-                    con3.query("INSERT INTO movies_post1980 (id, name, year, genre, director) VALUES (?,?,?,?,?);", values, function(err){
-                        if(err) throw err;
-                        console.log("Node 3: 1 record inserted");
-                        closeConnection(con3);
-                    });
-                });
-            };
-
-            closeConnection(con1);
-        });
+        closeConnection(con1);
     });
 }
 
