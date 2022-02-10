72c4f0e6e9a217ecd213519b3502107d428904e3
Raynier Chua <raynier_timothy_chua@dlsu.edu.ph>
2022-02-10 16:48:16 +0800

    added update function

--- db_dummy/db_functions.js
+++ db_dummy/db_functions.js
@@ -7,12 +7,6 @@ function closeConnection(con){
         console.log("Closed connection");
     });
 }
-// function updateOneRecord(name, year, genre, director){
-//     values = [name, year, genre, director];
-//     let query = "UPDATE movies_all SET 'name' = ?, 'year' = ?, 'genre' = ?, 'director' = ? WHERE 'id' = ?;"
-   
-//    //update node 2/3 get id then update node 1 
-// }
 
 function searchRecord(field, value){
     let query = "SELECT id, name, year, rank, genre, director FROM movies_all WHERE ? = ?;";
@@ -93,6 +87,53 @@ function insertOneRecordIntoAllNodes(name, year, genre, director){
                     con3.query("INSERT INTO movies_post1980 (id, name, year, genre, director) VALUES (?,?,?,?,?);", values, function(err){
                         if(err) throw err;
                         console.log("Node 3: 1 record inserted");
+                        closeConnection(con3);
+                    });
+                });
+            };
+
+            closeConnection(con1);
+        });
+    });
+}
+
+function updateOneRecordInAllNodes(id, name, year, genre, director){
+    // TODO: deal with situations when year is updated
+    values = [name, year, genre, director, id];
+
+    // update entry in node 1
+    con1.connect(function(err){
+        if(err) throw err;
+        console.log("Node 1: Connected");
+
+        con1.query("UPDATE movies_all SET name=?, year=?, genre=?, director=? WHERE id=?;", values, function(err, results){
+            if(err) throw err;
+            console.log("Node 1: updated " + results.affectedRows + " records");
+
+            // update entry in node 2
+            if(year < 1980){
+                con2.connect(function(err){
+                    if(err) throw err;
+                    console.log("Node 2: Connected");
+
+                    con2.query("UPDATE movies_pre1980 SET name=?, year=?, genre=?, director=? WHERE id=?;", values, function(err){
+                        if(err) throw err;
+                        console.log("Node 2: updated " + results.affectedRows + " records");
+                        closeConnection(con2);
+                    });
+                });
+            };
+
+
+            // update entry in node 3
+            if(year >= 1980){
+                con3.connect(function(err){
+                    if(err) throw err;
+                    console.log("Node 3: Connected");
+
+                    con3.query("UPDATE movies_post1980 SET name=?, year=?, genre=?, director=? WHERE id=?;", values, function(err){
+                        if(err) throw err;
+                        console.log("Node 3: updated " + results.affectedRows + " records");
                         closeConnection(con3);
                     });
                 });
