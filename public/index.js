dcc94056b2f3eb4fa7b8350f6aa2aba23d148a59
Ivan Lim <ivalim@ymail.com>
2022-02-13 21:05:28 +0800

    added some prep work to wiring frontend

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 
 var btn_add = document.getElementById("btn_modal_add");
 var span_add = document.getElementById("span_modal_add");
+var btn_modal_add = document.getElementById("btn_modal_add_movie");
+var btn_modal_update = document.getElementById("btn_modal_update_movie");
 
 var add_name = document.getElementById("add_name");
 var add_year = document.getElementById("add_year");
@@ -14,6 +16,16 @@ var span_report = document.getElementById("span_modal_report");
 var btn_edit = document.getElementById("btn_modal_edit");
 var span_edit = document.getElementById("span_modal_edit");
 
+var edit_name = document.getElementById("update_name");
+var edit_year = document.getElementById("update_year");
+var edit_rank = document.getElementById("update_rank");
+var edit_genre = document.getElementById("update_genre");
+var edit_director = document.getElementById("update_director");
+
+var btn_search = document.getElementById("btn_search")
+var search_field = document.getElementById("search_field");
+var search_value = document.getElementById("search_value");
+
 btn_add.onclick = function() {
     document.getElementById('modal_add').style.display='block';
 }
@@ -25,6 +37,19 @@ span_add.onclick = function() {
     add_rank.value = '';
     add_genre.value = '';
     add_director.value = '';
+}
+
+btn_modal_add.onclick = function() {
+    let data = {
+        name: add_name,
+        year: add_year,
+        rank: add_rank,
+        genre: add_genre,
+        director: add_director
+    };
+    $.post('/add_movie', data, function(res){
+        // idk prob show alert or something
+    });
 }
 
 btn_report.onclick = function() {
@@ -42,3 +67,27 @@ btn_edit.onclick = function() {
 span_edit.onclick = function() {
     document.getElementById('modal_edit').style.display='none'
 }
+
+btn_modal_update.onclick = function(){
+    let data = {
+        name: edit_name,
+        year: edit_director,
+        rank: edit_rank,
+        genre: edit_genre,
+        director: edit_director
+    };
+    $.post('edit_movie', data, function(res){
+
+    });
+}
+
+btn_search.onclick = function(){
+    let data = {
+        field : search_field,
+        value : search_value
+    };
+    $.get('/search_movies', data, function(res){
+        // show results
+    });
+}
+
