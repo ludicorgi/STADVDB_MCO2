122b4a8a0f46d5aeba948d9a0e56ac66a680dcdb
Ivan Lim <ivalim@ymail.com>
2022-02-16 12:16:36 +0800

    changed db credentials

--- db_dummy/db_connections.js
+++ db_dummy/db_connections.js
@@ -1,90 +1,91 @@
 var mysql = require('mysql');
 
 var con1 = mysql.createConnection({
-    host: "moves-all.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "35.198.223.213",
     port: 3306,
-    user: "admin",
-    password: "12345678",
+    user: "root",
+    password: "ADVANDBMPG9",
     database: "node1"
 });
 
 var con2 = mysql.createConnection({
-    host: "movies-left.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "34.96.224.7",
     port: 3306,
-    user: "admin",
-    password: "12345678",
+    user: "root",
+    password: "ADVANDBMPG9",
     database: "node2"
 });
 
 var con3 = mysql.createConnection({
-    host: "movies-right.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "34.101.134.145",
     port: 3306,
-    user: "admin",
-    password: "12345678",
+    user: "root",
+    password: "ADVANDBMPG9",
     database: "node3"
 });
 
 var con1Clone = mysql.createConnection({
-    host: "moves-all.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "35.198.223.213",
     port: 3306,
-    user: "admin",
-    password: "12345678",
+    user: "root",
+    password: "ADVANDBMPG9",
     database: "node1"
 });
 
 var con2Clone = mysql.createConnection({
-    host: "movies-left.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "34.96.224.7",
     port: 3306,
     user: "admin",
-    password: "12345678",
+    password: "ADVANDBMPG9",
     database: "node2"
 });
 
+
 var con3Clone = mysql.createConnection({
-    host: "movies-right.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "34.101.134.145",
     port: 3306,
-    user: "admin",
-    password: "12345678",
+    user: "root",
+    password: "ADVANDBMPG9",
     database: "node3"
 });
 
 var con3Clone2 = mysql.createConnection({
-    host: "movies-right.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+    host: "34.101.134.145",
     port: 3306,
-    user: "admin",
-    password: "12345678",
+    user: "root",
+    password: "ADVANDBMPG9",
     database: "node3"
 });
 
-function createConnectionNode3(){
+function createConnectionNode3() {
     return mysql.createConnection({
-        host: "movies-right.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+        host: "34.101.134.145",
         port: 3306,
-        user: "admin",
-        password: "12345678",
+        user: "root",
+        password: "ADVANDBMPG9",
         database: "node3"
     });
 }
 
-function createConnectionNode2(){
+function createConnectionNode2() {
     return mysql.createConnection({
-        host: "movies-left.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+        host: "34.96.224.7",
         port: 3306,
         user: "admin",
-        password: "12345678",
+        password: "ADVANDBMPG9",
         database: "node2"
     });
 }
 
-function createConnectionNode1 () {
+function createConnectionNode1() {
     return mysql.createConnection({
-        host: "moves-all.cxtjrbb21bon.ap-southeast-1.rds.amazonaws.com",
+        host: "35.198.223.213",
         port: 3306,
-        user: "admin",
-        password: "12345678",
+        user: "root",
+        password: "ADVANDBMPG9",
         database: "node1"
     });
 }
 
 
-module.exports = {con1, con2, con3, con1Clone, con2Clone, con3Clone, con3Clone2, createConnectionNode1, createConnectionNode2, createConnectionNode3};
+module.exports = { con1, con2, con3, con1Clone, con2Clone, con3Clone, con3Clone2, createConnectionNode1, createConnectionNode2, createConnectionNode3 };
